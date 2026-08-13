11919b32f9199d25f59ea3ed7cc11ccee10830d2
Amy Rowell <amymaria75@gmail.com>
2026-08-12 20:58:05 -0400

    Initial Commit

---
+++ DailyToDo/DailyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyToDoApp.swift
+//  DailyToDo
+//
+//  Created by Amy Rowell on 8/12/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
